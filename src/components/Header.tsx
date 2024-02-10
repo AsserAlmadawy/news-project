84710456ce37cb1fd8ebb07717ec1db618ae57a8
AsserAlmadawy <asser.almadawy@hotmail.com>
2024-02-10 22:20:26 +0200

    News Project

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import "@/styles/Header.css";
+
+export const Header: React.FC = () => (
+    <header>
+        <h1>News</h1>
+    </header>
+);
---
+++ src/components/News.tsx
@@ -0,0 +1,7 @@
+import "@/styles/News.css";
+
+export const News: React.FC = () => (
+    <div className="news">
+        <div className="news-card"></div>
+    </div>
+);
